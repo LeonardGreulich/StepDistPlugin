a1ab43ae64b6de779450b7cb571f071ccfb634ad
LeonardGreulich <l_greu02@uni-muenster.de>
2019-03-04 20:24:11 +0100

    Further configurations

--- src/android/DistanceService.java
+++ src/android/DistanceService.java
@@ -4,8 +4,8 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 
 import greulich.leonard.stepdist.MainActivity;
 import greulich.leonard.stepdist.R;
@@ -25,20 +25,18 @@ public class DistanceService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String test = intent.getStringExtra("Test");
+        
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Intent notificationIntent = new Intent(this, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, "stepDistServiceChannel")
+                .setContentTitle("Distance Service")
+                .setContentText(test)
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(pendingIntent)
+                .build();
 
-            Notification notification = new Notification.Builder(this, "stepDistServiceChannel")
-                    .setContentTitle("Distance Service")
-                    .setContentText(test)
-                    .setSmallIcon(R.drawable.ic_android)
-                    .setContentIntent(pendingIntent)
-                    .build();
-
-            startForeground(1, notification);
-        }
+        startForeground(1, notification);
 
         return START_REDELIVER_INTENT;
     }
