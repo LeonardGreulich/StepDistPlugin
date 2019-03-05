130bca96b49e88396cedd8b9ed7412e49a88cc57
LeonardGreulich <l_greu02@uni-muenster.de>
2019-03-05 11:37:22 +0100

    Preparations for android service

--- src/android/DistanceService.java
+++ src/android/DistanceService.java
@@ -4,46 +4,97 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 
 import greulich.leonard.stepdist.MainActivity;
 import greulich.leonard.stepdist.R;
 
-public class DistanceService extends Service {
+public class DistanceService extends Service implements LocationListener {
+
+    private LocationManager locationManager;
+    private final IBinder mBinder = new LocalBinder();
+
+    private Integer distanceFilter;
+    private Double accuracyFilter;
+    private Double perpendicularFilter;
+    private Integer locationsSequenceFilter;
+    private Double locationsSequenceDistanceFilter;
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        String test = intent.getStringExtra("Test");
-        
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
+        distanceFilter = intent.getIntExtra("distanceFilter", 0);
+        accuracyFilter = intent.getDoubleExtra("accuracyFilter", 0);
+        perpendicularFilter = intent.getDoubleExtra("perpendicularFilter", 0);
+        locationsSequenceFilter = intent.getIntExtra("locationsSequenceFilter", 0);
+        locationsSequenceDistanceFilter = intent.getDoubleExtra("locationsSequenceDistanceFilter", 0);
+
         Notification notification = new NotificationCompat.Builder(this, "stepDistServiceChannel")
                 .setContentTitle("Distance Service")
-                .setContentText(test)
+                .setContentText("The service is used to measure the traveled distance in the background.")
                 .setSmallIcon(R.drawable.ic_android)
                 .setContentIntent(pendingIntent)
                 .build();
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, distanceFilter, this);
+        } catch (SecurityException securityException) {
+            securityException.printStackTrace();
+        }
+
         startForeground(1, notification);
 
-        return START_REDELIVER_INTENT;
+        return mBinder;
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
+    public boolean onUnbind(Intent intent) {
+        locationManager.removeUpdates(this);
+        return super.onUnbind(intent);
+    }
+
+    public void startMeasuringDistance() {
+
+    }
+
+    public void stopMeasuringDistance() {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public class LocalBinder extends Binder {
+        public DistanceService getService(){
+            return DistanceService.this;
+        }
     }
 
 }
--- src/android/stepdistplugin.java
+++ src/android/stepdistplugin.java
@@ -3,62 +3,80 @@ package cordova.plugin.stepdist;
 import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.content.ServiceConnection;
 import android.os.Build;
-import android.os.Bundle;
+import android.os.IBinder;
 
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PermissionHelper;
 import org.apache.cordova.PluginResult;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static android.content.Context.LOCATION_SERVICE;
-
-/**
- * This class echoes a string called from JavaScript.
- */
-public class stepdistplugin extends CordovaPlugin implements LocationListener {
+public class stepdistplugin extends CordovaPlugin {
 
     private Context applicationContext;
-    private LocationManager locationManager;
+    private DistanceService distanceService;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            distanceService = ((DistanceService.LocalBinder)service).getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            distanceService = null;
+        }
+    };
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+
+        applicationContext = this.cordova.getActivity().getApplicationContext();
+        createNotificationChannel();
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("startLocalization")) {
-            String message = args.getString(0);
-            this.startLocalization(args, callbackContext);
+            startLocalization(args.getJSONObject(0), callbackContext);
+            return true;
+        } else if (action.equals("stopLocalization")) {
+            stopLocalization(callbackContext);
+            return true;
+        } else if (action.equals("startMeasuringDistance")) {
+            startMeasuringDistance(callbackContext);
+            return true;
+        } else if (action.equals("stopMeasuringDistance")) {
+            stopMeasuringDistance(callbackContext);
             return true;
         }
 
         return false;
     }
 
-    private void startLocalization(JSONArray args, CallbackContext callbackContext) {
-        applicationContext = this.cordova.getActivity().getApplicationContext();
-        createNotificationChannel();
-        locationManager = (LocationManager) applicationContext.getSystemService(LOCATION_SERVICE);
-
+    private void startLocalization(JSONObject args, CallbackContext callbackContext) throws JSONException {
         if (!PermissionHelper.hasPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
             PermissionHelper.requestPermission(this, 0, Manifest.permission.ACCESS_FINE_LOCATION);
         }
 
-        try {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        } catch (SecurityException securityException) {
-            securityException.printStackTrace();
-        }
-
         Intent serviceIntent = new Intent(applicationContext, DistanceService.class);
-        serviceIntent.putExtra("Test", "Test ForegroundService");
-        applicationContext.startService(serviceIntent);
+        serviceIntent.putExtra("distanceFilter", args.getInt("distanceFilter"));
+        serviceIntent.putExtra("accuracyFilter", args.getDouble("distanceFilter"));
+        serviceIntent.putExtra("perpendicularDistanceFilter", args.getDouble("distanceFilter"));
+        serviceIntent.putExtra("locationsSequenceFilter", args.getInt("distanceFilter"));
+        serviceIntent.putExtra("locationsSequenceDistanceFilter", args.getDouble("distanceFilter"));
+        applicationContext.bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
 
         JSONObject obj = new JSONObject();
         try {
@@ -74,8 +92,18 @@ public class stepdistplugin extends CordovaPlugin implements LocationListener {
     }
 
     private void stopLocalization(CallbackContext callbackContext) {
-        Intent serviceIntent = new Intent(applicationContext, DistanceService.class);
-        applicationContext.stopService(serviceIntent);
+        applicationContext.unbindService(serviceConnection);
+
+        PluginResult result = new PluginResult(PluginResult.Status.OK);
+        callbackContext.sendPluginResult(result);
+    }
+
+    private void startMeasuringDistance(CallbackContext callbackContext) {
+        distanceService.startMeasuringDistance();
+    }
+
+    private void stopMeasuringDistance(CallbackContext callbackContext) {
+        distanceService.stopMeasuringDistance();
     }
 
     private void createNotificationChannel() {
@@ -92,23 +120,4 @@ public class stepdistplugin extends CordovaPlugin implements LocationListener {
         }
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
 }
